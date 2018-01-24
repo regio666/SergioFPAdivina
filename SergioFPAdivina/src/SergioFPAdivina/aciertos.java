815b7a7dea40b1e80ea4a67f39157111b0563702
regio666 <regio@regioSSD-PC>
2018-01-24 16:02:47 +0100

    Correcciones en la documentación

--- SergioFPAdivina/src/SergioFPAdivina/aciertos.java
+++ SergioFPAdivina/src/SergioFPAdivina/aciertos.java
@@ -4,37 +4,39 @@ import java.util.Scanner;
 
 /**
  * @author Sergio Fernández Pina
- * @version 22.1 
+ * @version 23.1.2018 
  */
 
 public class aciertos {
 
     /**
-     * @param args (array de Strings obligatorio para el método main de java)
+     * Método "main" de java.
+     * @param args (array de Strings obligatorio para el método main de java).
      */
 	
     public static void main(String[] args) {
     	
     	/** 
-    	 * @param n =  variable que contiene el número introducido por teclado
-    	 * @param x = número aleatorio generado por Math.random
-    	 * @param cont = contador del numero de intentos
+    	 *  Variables necesarias para el funcionamiento del programa:
+    	 *  n =  variable que contiene el número introducido por teclado.
+    	 *  x = número aleatorio generado por Math.random.
+    	 *  cont = contador del numero de intentos.
     	 */
     	
         int n = 0,cont = 0;
         int x = (int) (500 *Math.random());
         
         /**
-         * Si descomentamos la siguiente línea veremos impreso el valor del número generado por Math.random
+         * Si descomentamos la siguiente línea veremos impreso en el terminal el valor del número generado por Math.random
          */
         //System.out.print(x);
         
         Scanner sc = new Scanner(System.in);
         
         /**
-         *  en el siguiente bucle do-while se comprueba si el número introducido es mayor n>x (Te pasaste amigo), 
-         *  o menor n<x (Casi cerca) y se añade 1 al parametro contador "cont", siempre que el número introducido sea distinto de x,
-         *  en el caso de ser el mismo número el bucle termina y continua el programa con el mensaje de acierto y el número del parametro contador
+         *  En el siguiente bucle do-while se comprueba si el número introducido es mayor "n>x" (Te pasaste amigo), 
+         *  o menor "n<x" (Casi cerca) y se añade 1 a la variable contador "cont", siempre que el número introducido en "n" sea distinto de "x",
+         *  en el caso de sea el mismo número el bucle termina y continua el programa con el mensaje de "Excelente..." y el número de la variable contador.
          */
         
         do {
