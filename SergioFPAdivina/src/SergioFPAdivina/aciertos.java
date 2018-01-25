25d445ee78a6ad172a070814134e2cefe732c0ce
regio666 <regio@regioSSD-PC>
2018-01-25 22:08:00 +0100

    Versión final - Ultimas modificaciones en Javadoc.

--- SergioFPAdivina/src/SergioFPAdivina/aciertos.java
+++ SergioFPAdivina/src/SergioFPAdivina/aciertos.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class aciertos {
 
     /**
-     * Método "main" de java.
+     * Método "main" de java, inicia el programa.
      * @param args (array de Strings obligatorio para el método main de java).
      */
 	
@@ -27,15 +27,15 @@ public class aciertos {
         int x = (int) (500 *Math.random());
         
         /**
-         * Si descomentamos la siguiente línea veremos impreso en el terminal el valor del número generado por Math.random
+         * Si descomentamos la siguiente línea veremos impreso en el terminal el valor del número generado por Math.random.
          */
         //System.out.print(x);
         
         Scanner sc = new Scanner(System.in);
         
         /**
-         *  En el siguiente bucle do-while se comprueba si el número introducido es mayor "n>x" (Te pasaste amigo), 
-         *  o menor "n<x" (Casi cerca) y se añade 1 a la variable contador "cont", siempre que el número introducido en "n" sea distinto de "x",
+         *  En el siguiente bucle do-while se comprueba si el número introducido es mayor "n>x" que el contenido en "x", 
+         *  o menor "n<x" y se añade 1 a la variable contador "cont", siempre que el número introducido en "n" sea distinto de "x",
          *  en el caso de sea el mismo número el bucle termina y continua el programa con el mensaje de "Excelente..." y el número de la variable contador.
          */
         
